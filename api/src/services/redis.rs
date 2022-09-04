6b7fe2a588ae4b4dbaa44a39f1901b5421e08411
Curtis <curtis@lassam.net>
2022-09-04 03:33:02 -0700

    add a redis reconnect policy

--- api/src/services/redis.rs
+++ api/src/services/redis.rs
@@ -1,6 +1,7 @@
 
 use fred::prelude::{RedisError, RedisConfig};
 use fred::pool::{RedisPool};
+use fred::types::ReconnectPolicy;
 
 
 const CACHE_POOL_SIZE: usize = 2;
@@ -9,8 +10,14 @@ pub async fn connect_redis(url: &String) -> Result<RedisPool, RedisError> {
     let config = RedisConfig::from_url(url)?;
     //redis|rediss[-cluster] :// [[username:]password@] host [:port][?[node=host1:port1][&node=host2:port2][&node=hostN:portN]]
     let pool = RedisPool::new(config, CACHE_POOL_SIZE)?;
-    let _ = pool.connect(None);
+    let policy = ReconnectPolicy::Constant { 
+        attempts: 0, 
+        max_attempts: 0, 
+        delay: 1000, 
+        jitter: 50 
+    };
+    let _ = pool.connect(Some(policy));
     let _ = pool.wait_for_connect().await?;
-  
+
     Ok(pool)
 }
